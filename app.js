64d42c6759b9a3e9e881d77643fb90098eb05215
Niall Holloway <niallholloway62@gmail.com>
2017-06-03 15:39:26 +0100

    Add Bootstrap and render new pages

--- app.js
+++ app.js
@@ -26,9 +26,12 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+//Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Home Route
 app.get('/', function(req, res) {
@@ -67,6 +70,16 @@ app.post('/articles/add',function(req, res){
     }
   });
 })
+
+//Get single article
+app.get('/article/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('article', {
+      article: article
+    });
+  })
+});
+
 //Start Server
 app.listen(3000, function() {
   console.log('Server started on port 3000');
